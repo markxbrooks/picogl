0e1a962b3f4c6f9384f2d0f465c5576bce5622a9
Mark Brooks <mark.x.brooks@gmail.com>
2025-08-13 08:57:35 -0400

    Update __init__.py

--- picogl/__init__.py
+++ picogl/__init__.py
@@ -1,2 +1,14 @@
-- __version__ = "0.1.0"
-- __all__ = ["backend", "buffers", "renderer", "shaders", "utils", "error", "frame", "info", "normals"]
+-__version__ = "0.1.0"
+
+__all__ = ["__version__", "RendererBase", "GLContext", "GLData", "VertexArrayObject"]
+
+def __getattr__(name):
+    if name in {"RendererBase", "GLContext", "GLData"}:
+        from importlib import import_module
+        mod = import_module("picogl.renderer")
+        return getattr(mod, name)
+    if name == "VertexArrayObject":
+        from importlib import import_module
+        mod = import_module("picogl.backend.modern.core.vertex.array.object")
+        return getattr(mod, name)
+    raise AttributeError(name)
